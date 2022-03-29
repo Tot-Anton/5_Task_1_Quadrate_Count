5345540b020c056ef4d6e4d02d0bb3751b8f5fbb
Anton Belmisov <antonbelmisov@mail.ru>
2022-03-29 17:50:32 +0300

    corrected the name

--- src/main/java/ru/netology/sqr/SqrService.java
+++ src/main/java/ru/netology/sqr/SqrService.java
@@ -1,7 +1,7 @@
 package ru.netology.sqr;
 
 public class SqrService {
-    public static int sqrOfRange(int lowerBound, int upperBound) {
+    public static int squaresInTheRange(int lowerBound, int upperBound) {
         int numberCounter = 0;
         for (int i = 10; i <= 99; i++) {
             if (lowerBound <= i * i && i * i <= upperBound) {
