8e3ab6ae700e459817bd70f49ccc120a992dd030
Anton Belmisov <antonbelmisov@mail.ru>
2022-03-29 18:07:42 +0300

    Fixed the tests.Please do not judge my English strictly, I learned German. I write via Yandex.

--- src/test/java/ru/netology/sqr/SqrServiceTest.java
+++ src/test/java/ru/netology/sqr/SqrServiceTest.java
@@ -6,18 +6,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SqrServiceTest {
 
-
     @org.junit.jupiter.api.Test
 
-
-    void squaresInTheRange() {
+    void squaresInTheRange1() {
         SqrService service = new SqrService();
-        int lowerBound = 200;
-        int upperBound = 300;
-        int expected = 3;
+        int lowerBound = 0;
+        int upperBound = 100;
+        int expected = 1;
         int actual = service.squaresInTheRange(lowerBound, upperBound);
         Assertions.assertEquals(expected, actual);
         System.out.println("Количество квадратов, тест 1: " + SqrService.squaresInTheRange(lowerBound , upperBound));
     }
 
+    @org.junit.jupiter.api.Test
+
+    void squaresInTheRange2() {
+        SqrService service = new SqrService();
+        int lowerBound = 100;
+        int upperBound = 500;
+        int expected = 13;
+        int actual = service.squaresInTheRange(lowerBound, upperBound);
+        Assertions.assertEquals(expected, actual);
+        System.out.println("Количество квадратов, тест 2: " + SqrService.squaresInTheRange(lowerBound , upperBound));
+    }
+
+    @org.junit.jupiter.api.Test
+
+    void squaresInTheRange3() {
+        SqrService service = new SqrService();
+        int lowerBound = 500;
+        int upperBound = 8000;
+        int expected = 67;
+        int actual = service.squaresInTheRange(lowerBound, upperBound);
+        Assertions.assertEquals(expected, actual);
+        System.out.println("Количество квадратов, тест 3: " + SqrService.squaresInTheRange(lowerBound , upperBound));
+    }
+
 }
+
+
